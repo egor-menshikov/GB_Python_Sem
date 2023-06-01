5c77f05d8adf03c8d2bdecedd67575bc613c6b77
Egor Menshikov <r3born.fs@gmail.com>
2023-06-01 15:19:40 +0300

    hw task_1 done

---
+++ HOMEWORKS/HW5/Task_1/main.py
@@ -0,0 +1,14 @@
+# Задача 26: Напишите программу, которая на вход принимает
+# два числа A и B, и возводит число А в целую степень B с
+# помощью рекурсии.
+
+def num_power(a, b):
+    if b == 1:
+        return a
+    if b == 0:
+        return 1
+    return a * num_power(a, b - 1)
+
+num = int(input('Введите число:\n'))
+power = int(input('Введите степень:\n'))
+print(num_power(num, power))
