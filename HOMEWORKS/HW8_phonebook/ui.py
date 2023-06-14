c8dafc744feedf761b6ae902fc85e3434af4eff3
Egor Menshikov <r3born.fs@gmail.com>
2023-06-14 18:47:41 +0300

    some reworks

---
+++ HOMEWORKS/HW8_phonebook/ui.py
@@ -0,0 +1,28 @@
+from logger import input_data, print_data, edit_data, delete_data
+
+
+def interface():
+    command = -1
+    while command != 5:
+        print('Доброго времени суток! Вы попали на специальную программу от нашей группы! Что же мы можем делать?\n'
+              '1. Добавить контакт\n'
+              '2. Удалить контакт\n'
+              '3. Редактировать контакт\n'
+              '4. Вывести данные\n'
+              '5. Выход')
+        command = int(input("Введите номер операции: "))
+
+        while command < 1 or command > 5:
+            print('Вы ошиблись при выборе.')
+            command = int(input("Введите номер операции: "))
+
+        if command == 1:
+            input_data()
+        elif command == 2:
+            delete_data()
+        elif command == 3:
+            edit_data()
+        elif command == 4:
+            print_data()
+        elif command == 5:
+            print("Спасибо, что воспользовались нашими услугами. Всего доброго!")
