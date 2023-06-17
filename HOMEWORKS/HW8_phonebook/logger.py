bf1fd6df741acfaea4413136c63fe527b4b71e02
Egor Menshikov <r3born.fs@gmail.com>
2023-06-17 10:58:24 +0300

    HW8 working on

--- HOMEWORKS/HW8_phonebook/logger.py
+++ HOMEWORKS/HW8_phonebook/logger.py
@@ -32,26 +32,41 @@ def input_data():
 
 def edit_data():
     phonebook = print_data()
+
     print(contact_choice := input('Введите номер записи, которую вы хотите изменить: '))
-    print(f'\nВы хотите изменить:\n\n'
-          f'{phonebook[contact_choice]["name"]:<12}'
-          f' {phonebook[contact_choice]["surname"]:<14}'
-          f' {phonebook[contact_choice]["phone"]:<16}'
-          f' {phonebook[contact_choice]["address"]:<16}')
+    while True:
+        if contact_choice.isdigit() and 0 < int(contact_choice) <= len(phonebook):
+            print(f'\nВы хотите изменить:\n\n'
+                  f'{phonebook[contact_choice]["name"]:<12}'
+                  f' {phonebook[contact_choice]["surname"]:<14}'
+                  f' {phonebook[contact_choice]["phone"]:<16}'
+                  f' {phonebook[contact_choice]["address"]:<16}')
+            break
+        else:
+            print(contact_choice := input('Введите номер записи, которую вы хотите изменить: '))
     print(f'{"1) Имя":<12}'
           f' {"2) Фамилия":<14}'
           f' {"3) Телефон":<16}'
           f' {"4) Адрес":<16}')
+
     print(column_choice := int(input('\nВыберите соответствующую категорию: ')))
-    match column_choice:
-        case 1:
-            phonebook[contact_choice]["name"] = name_data()
-        case 2:
-            phonebook[contact_choice]["surname"] = surname_data()
-        case 3:
-            phonebook[contact_choice]["phone"] = phone_data()
-        case 4:
-            phonebook[contact_choice]["address"] = address_data()
+    while True:
+        match column_choice:
+            case 1:
+                phonebook[contact_choice]["name"] = name_data()
+                break
+            case 2:
+                phonebook[contact_choice]["surname"] = surname_data()
+                break
+            case 3:
+                phonebook[contact_choice]["phone"] = phone_data()
+                break
+            case 4:
+                phonebook[contact_choice]["address"] = address_data()
+                break
+            case _:
+                print(column_choice := int(input('\nВыберите соответствующую категорию: ')))
+
     with open(PATH, 'w', encoding='utf-8') as file:
         json.dump(phonebook, file, ensure_ascii=False)
 
@@ -75,7 +90,6 @@ def search_data():
             if value.find(prompt) != -1:
                 print(f'{k:^2} {v["name"]:<12} {v["surname"]:<14} {v["phone"]:<16} {v["address"]:<16}')
                 break
-
 
 # delete_data()
 # input_data()
