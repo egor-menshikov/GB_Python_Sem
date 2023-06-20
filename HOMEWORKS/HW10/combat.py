169a78ef91e084d89eb8baaa38760b6f93a8523c
Egor Menshikov <r3born.fs@gmail.com>
2023-06-21 00:59:11 +0300

    HW10 in progress

--- HOMEWORKS/HW10/combat.py
+++ HOMEWORKS/HW10/combat.py
@@ -3,7 +3,8 @@ from fighter import Fighter
 
 def combat(one: Fighter, two: Fighter):
     if not one.isalive():
-        print(f'{one.name} is dead. {two} has {two.hp} health.')
+        print('======================================================================')
+        print(f'{one.name} is dead. {two.name} has {two.hp} health.')
         return
     one.strike(two)
     return combat(two, one)
